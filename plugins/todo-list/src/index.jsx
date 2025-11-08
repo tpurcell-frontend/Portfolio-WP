06399215accc1147c5e4eb869cdb1216042dcec8
Travis Purcell <travis.n.purcell@gmail.com>
2025-11-07 17:50:01 -0700

    Reconfigure the React mount from React 18 to React 17 syntax

--- plugins/todo-list/src/index.jsx
+++ plugins/todo-list/src/index.jsx
@@ -1,11 +1,9 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
+import {render} from '@wordpress/element';
 import App from './App'; 
 
 document.addEventListener('DOMContentLoaded', () => {
     const container = document.getElementById('todo-list-root');
     if (container) {
-        const root = createRoot(container); // React 18 root
-        root.render(<App />);
+        render(<App />, container);
     }
 });
